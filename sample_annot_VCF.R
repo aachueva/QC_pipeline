7be0255e338f94b2d26cfc47fe014d1aa771af16
Anastasia Chueva <aachueva@gcc-mac-008.in.gcc.biostat.washington.edu>
2016-07-25 10:52:13 -0700

    added subject id to sample annotation

--- sample_annot_VCF.R
+++ sample_annot_VCF.R
@@ -24,7 +24,7 @@ date()
 user<- try(system("echo $USER",intern = TRUE))
 
 annot<- get(load(path_sa_frz2))
-vcf_subset_by_project <- pData(annot[annot$PI==PI & annot$study==study ,])
+vcf_subset_by_project <- pData(annot[annot$PI==PI & annot$STUDY==study ,])
 
 sample  <- data.frame(vcf_subset_by_project$sample.id,stringsAsFactors=FALSE);
 meta    <- data.frame(labelDescription=c("NWD_ID unique scan ID within TOPMed project"), 
@@ -32,7 +32,17 @@ meta    <- data.frame(labelDescription=c("NWD_ID unique scan ID within TOPMed pr
 sample  <- AnnotatedDataFrame(sample,meta)
 names(pData(sample)) <-"sample.id"
 # dir.create(paste0(path_res_folder_dp,"/results"), showWarnings = TRUE, recursive = FALSE, mode = "0777")
-path_ann_df_vcf<- paste0(path_to_vcf_annot_folder,"/vcf.sample.v01.",user,".RData")
+path_ann_df_vcf<- paste0(path_to_sample_annot_folder_vcf,"/vcf.sample.v01.",user,".RData")
 
 save(sample,file=path_ann_df_vcf)
+
+#Adding subject_id
+
+colsToKeep <- c("sample.id","dbgap_subject_id")
+
+pData(sample) <- merge(pData(sample),pdata(annot)[,colsToKeep],by.x="sample.id");
+
 head(pData(sample))
+path_ann_df_vcf<- paste0(path_to_sample_annot_folder_vcf,"/vcf.sample.v02.",user,".RData")
+
+save(sample,file=path_ann_df_vcf)
